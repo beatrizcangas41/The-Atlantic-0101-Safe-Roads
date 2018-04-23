155b08708183578b799d92635e06d1eeea06bd0c
Nazik <nazikalm@gmail.com>
2018-04-23 15:25:33 -0400

    added sensors class

---
+++ sensors.cpp
@@ -0,0 +1,9 @@
+//
+//  sensors.cpp
+//  final project
+//
+//  Created by Nazik Almazova on 4/23/18.
+//  Copyright © 2018 Nazik Almazova. All rights reserved.
+//
+
+#include "sensors.h"
---
+++ sensors.h
@@ -0,0 +1,14 @@
+//
+//  sensors.h
+//  final project
+//
+//  Created by Nazik Almazova on 4/23/18.
+//  Copyright © 2018 Nazik Almazova. All rights reserved.
+//
+
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <iostream>
+
+#endif /* sensors_hpp */
